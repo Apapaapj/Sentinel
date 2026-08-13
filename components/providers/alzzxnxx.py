1a153383d4e39f9f216cb047282edeb4a5b1f2fc
Alsyahadat <arkanjagoan@gmail.com>
2026-08-13 15:46:23 +0700

    Add hacking message to alzzxnxx.py

---
+++ components/providers/alzzxnxx.py
@@ -0,0 +1,4 @@
+print ("Hello world")
+print ("My name is alzzxnxx")
+print ("Your site hasbeen hacked by älzzxnxx ga kill")
+print ("Thank you for reading this message, this is real proof that this website is very weak !!")
